ecbd413a23ab9b198369295c37f8cc56cb336c46
henz <hendrimardani290301@gmail.com>
2024-07-30 23:03:10 +0700

    change

--- app/src/main/java/com/example/mysubmission2/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/example/mysubmission2/ui/upcoming/UpcomingFragment.kt
@@ -6,10 +6,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.mysubmission2.databinding.FragmentUpcomingBinding
 import com.example.mysubmission2.ui.EventViewModel
 import com.example.mysubmission2.ui.ViewModelFactory
@@ -40,7 +40,11 @@ class UpcomingFragment : Fragment() {
         viewModel.getListEvents().observe(viewLifecycleOwner) { result ->
             if (result != null) {
                 when (result) {
+                    is Result.Loading -> {
+                        binding.progressBar.visibility = View.VISIBLE
+                    }
                     is Result.Success -> {
+                        binding.progressBar.visibility = View.GONE
                         val eventData = result.data
                         eventData.forEach {
                             Glide.with(this@UpcomingFragment)
@@ -50,13 +54,17 @@ class UpcomingFragment : Fragment() {
                             Log.e(TAG, it.name.toString())
                         }
                     }
-
-                    is Result.Error -> {}
-                    Result.Loading -> {}
+                    is Result.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(
+                            context,
+                            "Terjadi kesalahan" + result.error,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
         }
-
     }
 
     override fun onDestroyView() {
